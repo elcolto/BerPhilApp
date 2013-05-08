8d7b1cc6972df16c151e7c653f6a5f37a0d83af1
Sebastian Heeschen <sebastian.heeschen@gmail.com>
2013-05-08 16:00:16 +0200

    added randomized-position "bug". clear snap, info.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,8 +1,7 @@
+
 $(document).ready(function(){
 
 	
-
-
 	$('.animated_object').hammer().on('doubletap', function(ev){
 		ev.preventDefault();
 		$('#info').show('slow');
@@ -13,15 +12,37 @@ $(document).ready(function(){
 		$(this).fadeOut();
 	});
 
+	$('#bug').hammer().on('tap', function(ev){
+		ev.preventDefault();
+		$(this).fadeOut('slow');
+		clearTimeout();	
+	});
+
 	$(function(){
 		$('.get_pop').draggable({snap: '#snappable', snapMode:'inner'});
+		
 	});
+	var newPosition=function (){
+	var width = $(window).width();
+	var height = $(window).height();
+	var x = Math.random()*(width - 50);
+	var y = Math.random()*(height - 50);
+		
+		
+		$('#bug').css({'left':x,
+						'top':y});
+		$('#bug').show('slow');
+		//$('#bug').fadeOut('slow');
+
+	setTimeout(newPosition, 2000);
+	}
+	newPosition();
+
+
 });
 
 
 
-function rotate(degree){
-	$(this).css({
-		'transform': 'rotate(' + degrees + 'deg'
-	});
-}
+$(function(){
+	var rotateObject = $('#object5');
+});
